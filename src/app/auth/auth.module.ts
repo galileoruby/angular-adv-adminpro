61d4e538ef9682ea1e71285a8ccdb3b732e3e98a
Galileo Galván <galileo.galvan@interfactura.com>
2021-04-28 14:23:11 -0500

    Seccion 7: servicios, localstorage, rutas

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from '../app-routing.module';
+
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-import { AppRoutingModule } from '../app-routing.module';
 
 
 
 @NgModule({
   declarations:
-  [
+    [
       LoginComponent,
       RegisterComponent,
     ],
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    FormsModule
   ],
   exports: [
     LoginComponent,
